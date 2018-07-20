1e1ba596844292b9252be7588fba3b376729efc1
Udom Phunbuda <udom@aspiron.co.th>
2018-07-20 19:52:23 +0700

    Add bootstrap4 #1

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <template>
   <div id='app'>
-    <div id='nav'>
-      <router-link to='/'>Home</router-link> |
-      <router-link to='/about'>About</router-link>
-    </div>
-    <router-view/>
+    <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+      <a class='navbar-brand' href='#'>Navbar</a>
+      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarsExampleDefault' aria-controls='navbarsExampleDefault' aria-expanded='false' aria-label='Toggle navigation'>
+        <span class='navbar-toggler-icon'/>
+      </button>
+
+      <div id='navbarsExampleDefault' class='collapse navbar-collapse'>
+        <ul class='navbar-nav mr-auto'>
+          <li class='nav-item active'>
+            <a class='nav-link' href='#'>Home <span class='sr-only'>(current)</span></a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link' href='#'>Link</a>
+          </li>
+          <li class='nav-item'>
+            <a class='nav-link disabled' href='#'>Disabled</a>
+          </li>
+          <li class='nav-item dropdown'>
+            <a id='dropdown01' class='nav-link dropdown-toggle' href='http://example.com' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Dropdown</a>
+            <div class='dropdown-menu' aria-labelledby='dropdown01'>
+              <a class='dropdown-item' href='#'>Action</a>
+              <a class='dropdown-item' href='#'>Another action</a>
+              <a class='dropdown-item' href='#'>Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class='form-inline my-2 my-lg-0'>
+          <input class='form-control mr-sm-2' type='text' placeholder='Search' aria-label='Search'>
+          <button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>
+        </form>
+      </div>
+    </nav>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,33 @@
+const webpack = require("webpack");
+
 module.exports = {
   baseUrl: undefined,
   outputDir: undefined,
   assetsDir: undefined,
   runtimeCompiler: true,
-  productionSourceMap: undefined,
+  productionSourceMap: false,
   parallel: undefined,
 
   css: {
     modules: true,
-    sourceMap: true
+    sourceMap: true,
+    loaderOptions: {
+      // pass options to sass-loader
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        data: `@import "@/style/variables.scss";
+              @import "~bootstrap/scss/bootstrap.scss";`
+      }
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        Popper: "popper.js"
+      })
+    ]
   }
 };

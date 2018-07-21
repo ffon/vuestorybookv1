77a3aa1d9e708dfa6f81334fc80f1dc7f4bae517
BIRD-MAC <bird.urights@gmail.com>
2018-07-21 21:21:06 +0700

    add router

--- src/App.vue
+++ src/App.vue
@@ -8,32 +8,32 @@
 
       <div id="navbarsExampleDefault" class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+
+          <li class="nav-item">
+            <router-link class="nav-link"  to="/">Home <span class="sr-only">(current)</span></router-link> 
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <router-link class="nav-link"  to="/about">About</router-link>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a id="dropdown01" class="nav-link dropdown-toggle" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
+
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        
       </div>
     </nav>
+    <div class="container" style="margin-top:80px;">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+#app {
+  text-align: center;
+  nav {
+    .router-link-exact-active {
+      font-weight: 500 !important;
+      color: #fff;
+    }
+  }
+}
 </style>
